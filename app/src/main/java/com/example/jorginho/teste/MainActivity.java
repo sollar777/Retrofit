d52021b088779eaeb213d5d2bce58df5b5bffa88
JorgeFerreira <30783497+sollar777@users.noreply.github.com>
2018-12-25 15:45:15 -0300

    primeiro commit

--- app/src/main/java/com/example/jorginho/teste/MainActivity.java
+++ app/src/main/java/com/example/jorginho/teste/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String name;
+    private String texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
